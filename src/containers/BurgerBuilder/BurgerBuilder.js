cdb013387d7122d8957f3c75c4a83d418f0c3651
Nemanja Mirosavljev <n.mirosavljev@levi9.com>
2020-10-01 13:05:16 +0200

    feat(all): added authentification and route guards

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -77,7 +77,12 @@ class BurgerBuilder extends Component {
   }
 
   purchaseHandler = () => {
-    this.setState({purchasing: true});
+    if (this.props.isAuthenticated) {
+      this.setState({purchasing: true});
+    } else {
+      this.props.onSetAuthRedirectPath('/checkout');
+      this.props.history.push('/auth');
+    }
   }
 
   purchaseCancelHandler = () => {
@@ -147,8 +152,9 @@ class BurgerBuilder extends Component {
               ingredientRemoved={this.props.onIngredientRemoved}
               disabled={disabledInfo}
               purchaseable={this.updatePurchaseState(this.props.ings)}
-              price={this.props.price}
               ordered={this.purchaseHandler}
+              isAuth={this.props.isAuthenticated}
+              price={this.props.price}
               />
         </Aux>
       );
@@ -174,7 +180,8 @@ const mapStateToProps = state => {
   return {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    error: state.burgerBuilder.error
+    error: state.burgerBuilder.error,
+    isAuthenticated: state.auth.token !== null
   }
 };
 
@@ -184,6 +191,7 @@ const mapDispatchToProps = dispatch => {
     onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
     onInitIngredients: () => dispatch(actions.initIngredients()),
     onInitPurchase: () => dispatch(actions.purchaseInit()),
+    onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path)),
   }
 };
 
